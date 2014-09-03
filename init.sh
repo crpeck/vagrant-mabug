b16486a6eb5a797e7d975e61a23e75ddfbae1ef5
Chris Peck <crpeck@wm.edu>
2014-09-03 14:00:53 -0400

    refactored configuring jenkins during provisioning

--- init.sh
+++ init.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 date
 if [ -f /vagrant/init.ran ]; then
-  echo "init.sh has run, remove init.ran to run it again"
+  echo -e "\ninit.sh has already run, remove init.ran to run it again\n"
   exit 0
 fi
 echo "Provisioning....."
 sudo apt-get update
-echo "Installing ruby-dev"
+echo -e "\nInstalling ruby-dev"
 sudo apt-get install -y ruby-dev
-echo "Installing librarian-puppet"
+echo -e "\nInstalling librarian-puppet"
 sudo gem install librarian-puppet
 cd /vagrant/puppet
-echo "Installing puppet modules from Puppetfile"
+echo -e "\nInstalling puppet modules from Puppetfile"
 librarian-puppet install
+echo -e "\nCreating file init.ran, to run init.sh during provisioning again you"
+echo "must remove the file named init.ran"
 date > /vagrant/init.ran
+echo -e "\ninit.sh finished\n"
 exit 0
--- post-install.sh
+++ post-install.sh
@@ -2,9 +2,21 @@
 #
 # run this after puppet provision is finished
 #
-# automagically create a 'hello-world' job in jenkins
-cd /var/lib/jenkins/jobs
-sudo tar xf /vagrant/hello-world.tar
-sudo chown jenkins:jenkins hello-world
+# this copies anything under the jenkins-files diretory
+# here over to /var/lib/jenkins on the vagrant box
+#
+# updates jeknins main config file and configures maven
+# automagically creates a 'hello-world' job in jenkins
+#
+echo -e "\nStarting Post Install provisioning"
+echo -e "\nConfiguring Jenkins"
+cd /vagrant/jenkins-files
+tar cf - . | (cd /var/lib/jenkins; sudo tar xf -)
+sudo chown -R jenkins:jenkins /var/lib/jenkins
+echo -e "\nRestarting Jenkins"
 sudo service jenkins restart
+echo "Restarting Tomcat"
 sudo service tomcat7 restart
+echo -e "\nJenkins is available at http://localhost:8090"
+echo "Tomcat manager at http://localhost:8080/manager"
+echo "  user=manager, password=12345"
