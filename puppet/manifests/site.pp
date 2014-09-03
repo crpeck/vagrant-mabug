c59992b2633a37ed0e699ac14fe6414255c8ed2b
Chris Peck <crpeck@wm.edu>
2014-09-03 15:45:45 -0400

    update spacing

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -12,7 +12,7 @@ class { '::tomcat7':
 class { 'jenkins':
   configure_firewall => false,
 
-  config_hash   => {
+  config_hash        => {
     'HTTP_PORT' => { 'value' => '8090' },
     'AJP_PORT'  => { 'value' => '8099' },
   },
@@ -43,9 +43,9 @@ package { 'curl':
 }
 
 vcsrepo { '/usr/local/src/hello-world':
+  ensure   => 'present',
   owner    => 'vagrant',
   group    => 'vagrant',
-  ensure   => 'present',
   provider => git,
   source   => 'https://github.com/crpeck/hello-world.git',
   require  => Class['::jenkins'],
@@ -53,25 +53,26 @@ vcsrepo { '/usr/local/src/hello-world':
 }
 
 file { '/usr/local/src/hello-world/.git/hooks/post-commit':
+  ensure  => present,
   owner   => 'vagrant',
   group   => 'vagrant',
   mode    => '0755',
-  ensure  => present,
   content => "curl -X POST http://localhost:8090/job/hello-world/build?delay=0sec\n",
 }
 
 file { '/home/vagrant/.gitconfig':
+  ensure  => present,
   owner   => 'vagrant',
   group   => 'vagrant',
   mode    => '0644',
-  ensure  => present,
   content => "[user]\n email = vagrantuser@some.where\n name = Vagrant User\n",
 }
 file { '/var/lib/tomcat7/webapps/ROOT/index.html':
+  ensure  => present,
   owner   => 'tomcat7',
   group   => 'tomcat7',
   mode    => '0644',
-  ensure  => present,
+  require => Class['::tomcat7'],
   content => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> <head> <title>Jenkins Demo</title> </head> <body> <h1>It works !</h1> Click here to see the <a href="/hello-world">hello-world</a>i example'
 }
 
