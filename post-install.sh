7c2afd33c8c0b04354ca3ce16b9d1a86f1e40685
Chris Peck <crpeck@wm.edu>
2014-09-03 07:04:48 -0400

    autocreate a jenkins job

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,13 @@
 # run this after puppet provision is finished
 #sudo service tomcat7 restart
 #sudo service jenkins start
+# copy post-commit script to source tree for hello-world if needed
+if [ -r /tmp/hello-world/.git/hooks/post-commit ]; then
+  cp /vagrant/post-commit /tmp/hello-world/.git/hooks 
+fi
+#
+# automagically create a 'hello-world' job in jenkins
+cd /var/lib/jenkins/jobs
+sudo tar xf /vagrant/hello-world-jenkins.tar
+sudo chown jenkins:jenkins hello-world
+sudo service jenkins restart
