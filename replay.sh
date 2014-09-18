4a64e8890c3cfdc4ffeea35cd43dd916809d4b16
Chris Peck <crpeck@wm.edu>
2014-09-18 15:00:29 -0400

    check if box is up when runing replay.sh

--- replay.sh
+++ replay.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # run scriptreplay on the vagrantbox - speed up replay time by 100
+#
+# verify the box is running
+vagrant status|grep -q running
+if [ $? -ne 0 ]; then
+  echo "This Vagrant Box is not running, unfortunately"
+  echo "replay cannot run without it being up"
+  exit 1
+fi
 vagrant ssh -c "scriptreplay -t /vagrant/vagrant-up.tm /vagrant/vagrant-up.out 100"
 exit
